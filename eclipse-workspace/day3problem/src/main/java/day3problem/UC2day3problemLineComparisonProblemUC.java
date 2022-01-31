44e9307371da60af24e12d31fe81cccf5b551795
ajinkyakambe <ajinkyakambe@gmail.com>
2022-01-31 18:47:27 +0530

    Update UC2day3problemLineComparisonProblemUC.java

--- eclipse-workspace/day3problem/src/main/java/day3problem/UC2day3problemLineComparisonProblemUC.java
+++ eclipse-workspace/day3problem/src/main/java/day3problem/UC2day3problemLineComparisonProblemUC.java
@@ -2,21 +2,38 @@ package day3problem;
 
 public class UC2day3problemLineComparisonProblemUC {
 
-
-	  // Function to calculate distance
-	static double distancecount(int x1, int y1, int x2, int y2)
+	// Method to check if they are identical
+	static void lineequality(double a1, double b1,
+						double c1, double a2,
+						double b2, double c2)
 	{
-	    // Calculating distance
-	    return Math.sqrt(Math.pow(x2 - x1, 2) +
-	                Math.pow(y2 - y1, 2) * 1.0);
+		
+		// Converison from float to string
+		String co1 = Double.toString(a1 / a2);  
+		String co2 = Double.toString(b1 / b2); 
+		String co3 = Double.toString(c1 / c2); 
+		
+				
+     // Equality check based on formula
+		if (( co1.equals(co2))
+			&& (co1.equals(co3))
+			&& (co2.equals(co3)))
+			System.out.println( "The given straight"
+			+" lines are identical");
+
+		else
+			System.out.println("The given straight"
+				+ " lines are not identical");
 	}
 
-		public static void main(String[] args) {
-			// TODO Calculating distance calling
-			
-		 System.out.println(distancecount(5, 7, 3, 3));
-
-		}
-
+	// Calling Code
+	public static void main(String[] args)
+	{
+		double a1 = -2, b1 = 4,
+				c1 = 3, a2 = -6,
+				b2 = 12, c2 = 9;
+		lineequality(a1, b1, c1, a2, b2, c2);
+	}
+	
 	
 }
